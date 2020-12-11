ca6824fdc769d4a2debc096692570f7a883396cc
arunkumarcs <arunkumarmessenger@gmail.com>
2020-12-12 01:46:44 +0530

    all stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,8 @@ module.exports = {
   stories: [
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../pages/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
